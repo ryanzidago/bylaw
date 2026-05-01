5bc05b8fd9dbdd55f75670dccb54444dbf03ece4
Ryan Zidago <ryanzidago@gmail.com>
2026-05-01 21:28:52 +0300

    first commit

---
+++ lib/bylaw.ex
@@ -0,0 +1,18 @@
+defmodule Bylaw do
+  @moduledoc """
+  Documentation for `Bylaw`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Bylaw.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/bylaw_test.exs
@@ -0,0 +1,8 @@
+defmodule BylawTest do
+  use ExUnit.Case
+  doctest Bylaw
+
+  test "greets the world" do
+    assert Bylaw.hello() == :world
+  end
+end
