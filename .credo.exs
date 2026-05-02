3a1ff4da1e493e6540bf9593db991dbb4cd1fe3a
Ryan Zidago <ryanzidago@gmail.com>
2026-05-02 14:00:47 +0300

    Remove CaseToWith Credo check

--- .credo.exs
+++ .credo.exs
@@ -191,7 +191,6 @@
           {Credo.Check.Refactor.UnlessWithElse, []},
           {Credo.Check.Refactor.UtcNowTruncate, []},
           {Credo.Check.Refactor.WithClauses, []},
-          {Bylaw.Credo.Check.Refactor.CaseToWith, false},
           {Bylaw.Credo.Check.Refactor.FilterRejectFirst, []},
           {Bylaw.Credo.Check.Refactor.NoNestedCase, false},
           {Bylaw.Credo.Check.Refactor.RejectCount, []},
