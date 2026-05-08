28974ab88e0ac97931b50c4f342701e16aa2a372
Ryan Zidago <ryanzidago@gmail.com>
2026-05-08 07:31:26 +0200

    Fix Hex package docs

--- packages/bylaw_ecto_query/lib/bylaw/ecto/query/checks.ex
+++ packages/bylaw_ecto_query/lib/bylaw/ecto/query/checks.ex
@@ -1,3 +1,8 @@
 defmodule Bylaw.Ecto.Query.Checks do
-  @moduledoc false
+  @moduledoc """
+  Namespace for built-in `Bylaw.Ecto.Query.Check` implementations.
+
+  Use the individual check module pages for rule-specific examples, accepted
+  query shapes, limitations, and issue metadata.
+  """
 end
