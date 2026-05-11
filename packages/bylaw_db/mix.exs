90754bad2d2206c0fd8956453d7ec25885e013d7
Ryan Zidago <46972947+ryanzidago@users.noreply.github.com>
2026-05-11 07:35:32 +0200

    Prepare bylaw_core alpha release
    Prepare bylaw_core 0.1.0-alpha.1 and align dependent constraints with the alpha release.

--- packages/bylaw_db/mix.exs
+++ packages/bylaw_db/mix.exs
@@ -30,7 +30,7 @@ defmodule BylawDb.MixProject do
 
   defp deps do
     [
-      {:bylaw_core, "~> 0.1", hex: :bylaw_core, path: "../bylaw_core"},
+      {:bylaw_core, "~> 0.1.0-alpha.1", hex: :bylaw_core, path: "../bylaw_core"},
       {:bylaw_credo, "== 0.1.0-alpha.1", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.39", only: [:dev, :test], runtime: false},
       {:usage_rules, "~> 1.2", only: :dev, runtime: false}
