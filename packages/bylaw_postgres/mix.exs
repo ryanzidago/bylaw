f58db00979049dc3582615edf4ed48b92dcdd008
Ryan Zidago <ryanzidago@gmail.com>
2026-05-11 08:45:23 +0300

    Document bylaw_core path dependency

--- packages/bylaw_postgres/mix.exs
+++ packages/bylaw_postgres/mix.exs
@@ -51,6 +51,7 @@ defmodule BylawPostgres.MixProject do
 
   defp deps do
     [
+      # Required for local path builds because bylaw_db starts :bylaw_core.
       {:bylaw_core, "~> 0.1.0-alpha.1", hex: :bylaw_core, path: "../bylaw_core"},
       {:bylaw_credo, "== 0.1.0-alpha.1", only: [:dev, :test], runtime: false},
       {:bylaw_db, "~> 0.1", hex: :bylaw_db, path: "../bylaw_db"},
