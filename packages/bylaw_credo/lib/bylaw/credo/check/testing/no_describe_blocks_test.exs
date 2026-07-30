4e644454f0688e5468c2aa0e200674f99fcff433
Ryan Zidago <ryanzidago@gmail.com>
2026-07-30 16:03:35 +0300

    Draft no describe blocks acceptance tests

---
+++ packages/bylaw_credo/lib/bylaw/credo/check/testing/no_describe_blocks_test.exs
@@ -0,0 +1,24 @@
+defmodule Bylaw.Credo.Check.Testing.NoDescribeBlocksTest do
+  use Credo.Test.Case
+
+  test "reports describe blocks in test files" do
+  end
+
+  test "reports every describe block in a test file" do
+  end
+
+  test "reports nested describe blocks" do
+  end
+
+  test "recommends descriptive standalone test names and multiple focused test files" do
+  end
+
+  test "does not report describe calls outside test files" do
+  end
+
+  test "does not report identifiers containing describe" do
+  end
+
+  test "respects excluded paths" do
+  end
+end
